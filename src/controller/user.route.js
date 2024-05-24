96924cbf9c271879c16d4c94929aaa08557d77b4
vimal-oneway <vimalic555@gmail.com>
2024-05-24 15:18:53 +0530

    user abstraction

--- src/controller/user.route.js
+++ src/controller/user.route.js
@@ -5,6 +5,7 @@ import { UserModel } from "../database/model/user.model.js";
 router.post("/login", async(req, res)=>{
     const {email, password, username} = req.body;
 
+    // Sign Up
     if(username!=""){
         let newUser = await UserModel.create({
             email:email,
@@ -12,25 +13,25 @@ router.post("/login", async(req, res)=>{
             username:username
         });
 
-
         if(!newUser) {
             return res.json({message:"Unable to create an user", success:false});
         }
 
-// 
-// const {password ps, ...userwithoutpassword} = newUser;
+        const {password: ps, ...newUserWithoutPassword} = newUser;
 
-        return res.json({message:"Successfully created user",user: newUser , success:true});
+        return res.json({message:"Successfully created user",user: newUserWithoutPassword , success:true});
     }
 
 
+    // login
     const exitingUser = await UserModel.findOne({email:email, password:password});
 
     if(!exitingUser) {
-        return res.json({message:"User not found 404", success:false});
+        return res.json({message:"Email or password are incorrect", success: false});
     }
 
-    return res.json({message:"Successfully logged In.",user:exitingUser, success:true});
+    return res.json({message:"Successfully logged In.", user: exitingUser, success: true});
 
 })
+
 export default router;
--- src/database/model/user.model.js
+++ src/database/model/user.model.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema  = mongoose.Schema({
     email:{type:String, require:true},
     username:{type:String, require:true},
-    password:{type:String, require:true},
+    password:{type:String, require:true, select:false},
     createdAt:{type:Date, default: Date.now()}
 })
 
-export const UserModel = mongoose.model("user", userSchema); 
+export const UserModel = mongoose.model("user", userSchema);
